036c5f673c5e89270579ad860f5476a5cfb6281b
nitheesh-1612 <116325479+nitheesh-1612@users.noreply.github.com>
2022-11-10 01:54:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! how are you and this is a master ");
+    System.out.println("Hello world! how are you and this is a master");
   }
 }
